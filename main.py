a691fce47b7c9c7b84acf299385fcedbce44f077
Aman Juneja <71624646+amankn@users.noreply.github.com>
2021-10-26 18:29:37 +0000

    basic percentage caluclation

--- main.py
+++ main.py
@@ -6,3 +6,20 @@
 #Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
 
 #Write your code below this line 👇
+
+print(12 /100)
+
+print(150 * 0.12)
+
+print(150 + 18)
+
+print(150 * 1.12)
+
+# 10% of hundred + 100, 1 represent the number itself
+print(100 * 1.10)
+
+bill = 150
+
+tip = (bill / 15) * 1.10
+
+print(tip)
